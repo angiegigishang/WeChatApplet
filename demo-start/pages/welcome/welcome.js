8e84ba8a3ba5dac17d7b4cbb06abc637f272e2c2
shangxy <shangxy@zjrealtech.com>
2018-12-07 17:11:12 +0800

    database transportation

--- demo-start/pages/welcome/welcome.js
+++ demo-start/pages/welcome/welcome.js
@@ -3,14 +3,5 @@ Page({
      wx.redirectTo({
        url: "../posts/post"
      })   
-  },
-  onSubTap: function(event) {
-    console.log('execute onSubTap')
-  },
-  onUnload: function() {
-    //console.log('welcome page is onunload')
-  },
-  onHide: function() {
-    //console.log('welcome page is hide')
   }
 })
