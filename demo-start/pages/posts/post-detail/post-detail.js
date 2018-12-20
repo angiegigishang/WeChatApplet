8875ddf1ad4d4539d085086e854cdb1956ac4dfe
shangxy <shangxy@zjrealtech.com>
2018-12-20 11:21:42 +0800

    收藏切换弹出框逻辑修改

--- demo-start/pages/posts/post-detail/post-detail.js
+++ demo-start/pages/posts/post-detail/post-detail.js
@@ -2,7 +2,7 @@ var postsData = require('../../../data/posts-data.js')
 
 Page({
   data: {
-      
+
   },
   onLoad: function(option) {
     var postId = option.id;
@@ -10,10 +10,10 @@ Page({
     var postData = postsData.postList[postId];
     //如果在onload方法中，不是异步的去执行一个数据绑定，
     //则不需要使用this.setData方法，只需要对this.data赋值即可实现数据绑定
-    this.setData ({
+    this.setData({
       postData: postData
     })
-    
+
     // var postsCollected = {
     //   1: "true",
     //   2: "false",
@@ -21,17 +21,16 @@ Page({
     // }
 
     var postsCollected = wx.getStorageSync('posts_collected')
-    if(postsCollected) {
+    if (postsCollected) {
       var postCollected = postsCollected[postId]
       this.setData({
         collected: postCollected
       })
-    }
-    else {
+    } else {
       var postsCollected = {};
       postsCollected[postId] = false;
       wx.setStorageSync('posts_collected', postsCollected);
-    } 
+    }
   },
 
   onCollectionTap: function(event) {
@@ -40,27 +39,44 @@ Page({
     //收藏转换
     postCollected = !postCollected;
     postsCollected[this.data.currentPostId] = postCollected;
+    this.showModal(postsCollected, postCollected);
+
+  },
+
+  showModal: function (postsCollected, postCollected) {
+    var that = this;
+    wx.showModal({
+      title: '收藏',
+      content: postCollected ? '收藏该文章' : '取消收藏该文章',
+      showCancel: "true",
+      cancelText: "取消",
+      cancelColor: '#333',
+      confirmText: '确认',
+      confirmColor: '#405f80',
+      success: function(res) {
+        if(res.confirm) {
+          //更新文章是否的缓存值
+          wx.setStorageSync('posts_collected', postsCollected);
+          //更新数据绑定,从而实现切换照片
+          that.setData({
+            collected: postCollected
+          })
+        }
+      }
+    })
+  },
+
+  showToast: function (postsCollected, postCollected) {
     //更新文章是否的缓存值
     wx.setStorageSync('posts_collected', postsCollected);
     //更新数据绑定,从而实现切换照片
     this.setData({
       collected: postCollected
     })
-
-    // wx.showToast({
-    //   title: postCollected ? '收藏成功' : '取消成功',
-    //   duration: 1000,
-    //   icon: 'success'
-    // })
-
-    wx.showModal({
-      title:'收藏',
-      content: '是否收藏该文章',
-      showCancel: "true",
-      cancelText: "不收藏",
-      cancelColor: '#333',
-      confirmText: '收藏',
-      confirmColor: '#405f80'
+    wx.showToast({
+      title: postCollected ? '收藏成功' : '取消成功',
+      duration: 1000,
+      icon: 'success'
     })
   }
 
