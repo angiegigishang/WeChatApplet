28b45dccc3eeea3e48ec13a1c232d9359ad0854d
shangxy <shangxy@zjrealtech.com>
2018-12-21 13:50:20 +0800

    新闻列表页面完成

--- demo-start/pages/posts/post.js
+++ demo-start/pages/posts/post.js
@@ -21,6 +21,15 @@ Page({
   },
   onSwiperItemTap: function(event) {
     var postId = event.currentTarget.dataset.postid;
+    // wx.navigateTo({
+    //   url: "post-detail/post-detail?id=" + postid
+    // })
+  },
+  onSwiperTap: function(event) {
+    //target与currentTarget区别，target指当前点击组件，currentTarget指事件捕获的组件
+    //target这里指的是image而current指的是swiper
+    var postId = event.target.dataset.postid;
+    //console.log(postId);
     wx.navigateTo({
       url: "post-detail/post-detail?id=" + postId
     })
