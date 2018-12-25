6e55ecf03da0f8d72a64728088da06eab0fb82bd
shangxy <shangxy@zjrealtech.com>
2018-12-25 14:22:26 +0800

    电影页面获取api接口数据

--- demo-start/pages/movies/movies.js
+++ demo-start/pages/movies/movies.js
@@ -1,65 +1,26 @@
+var app = getApp();
+
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-    
+  onLoad: function(event) {
+    var inTheatersUrl = app.globalData.doubanBase + '/movies.today?key=1a08b87e13b7f9bec5d8d2868e484718e&cityid=10';
+    var comingSoonUrl = app.globalData.doubanBase+'/movies.today?key=1a08b87e13b7f9bec5d8d2868e484718e&cityid=10';
+    var top250Url = app.globalData.doubanBase +'/movies.today?key=1a08b87e13b7f9bec5d8d2868e484718e&cityid=10'; 
+    this.getMovieListData(inTheatersUrl);
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-    
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-    
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-    
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-    
+  getMovieListData: function(url) {
+    wx.request({
+      url: url,
+      method: "GET",
+      header: {
+        "Content-Type": ""
+      },
+      success: function (res) {
+        console.log(res)
+      },
+      fail: function () {
+        console.log('请求失败')
+      }
+    })
   }
 })
